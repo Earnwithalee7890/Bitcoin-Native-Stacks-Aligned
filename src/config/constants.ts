95b5f0384402021726cf49a11aca06bce78ba955
Earnwithalee7890 <earnwithalee@gmail.com>
2025-12-30 00:44:12 +0500

    chore: create centralized app configuration in src/config/constants.ts

---
+++ src/config/constants.ts
@@ -0,0 +1,8 @@
+export const STACKS_CONTRACT_ADDRESS = "SP2F500B8DTRK1EANJQ054BRAB8DDKN6QCMXGNFBT";
+export const STACKS_CONTRACT_NAME = "check-in";
+export const APP_NAME = "Bitcoin-Native & Stacks-Aligned";
+export const APP_DESCRIPTION = "The ultimate builder command center for the Stacks ecosystem.";
+export const HIRO_API_BASE = "https://api.mainnet.hiro.so";
+export const COINGECKO_API_BASE = "https://api.coingecko.com/api/v3";
+export const SOCIAL_GITHUB = "https://github.com/Earnwithalee7890";
+export const TALENT_PROTOCOL_URL = "https://talent.app/aleekhoso";
